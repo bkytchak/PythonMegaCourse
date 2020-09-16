1fd397990df354fdf2d6ec641394cf89ffcddea0
bkytchak <bkytchak@gmail.com>
2020-09-15 20:10:21 -0400

    Added code to get close matches of entered word.

--- EnglishThesarus/EnglishThesarus.py
+++ EnglishThesarus/EnglishThesarus.py
@@ -3,11 +3,19 @@
 #Project: Creating a thesarus using python
 
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+    else:
+        return "The word doesn't exist. Please double check spelling"
+
 
 word = input("Enter word: ")
 
